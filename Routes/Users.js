981d010a159e8292fae2ecc4630ca59cd16cfc93
Salamanilyas <95139252+SalmanIlya@users.noreply.github.com>
2022-11-30 11:10:09 +0500

    create-routes-for-user

--- Routes/Users.js
+++ Routes/Users.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const Users = require("../Controllers/Users");
+const {
+  verifyTockenandAuth,
+  verifitocken,
+  verifyAdminTockenandAuth,
+} = require("../middlewear/VerifiTocken");
+
 const route = express.Router();
 
 route.post("/user", Users.userRouter);
 route.post("/login", Users.LoginUser);
-route.get("/user", Users.allUser);
+route.put("/:id", verifyTockenandAuth, Users.UpdateUser);
+route.delete("/:id", verifyTockenandAuth, Users.deleteUser);
+route.get("/user/:id", verifyAdminTockenandAuth, Users.getOneUser);
+route.get("/alluser", verifyAdminTockenandAuth, Users.allUser);
 
 module.exports = route;
