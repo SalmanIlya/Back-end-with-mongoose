6481e0c03fb7aa0dc0beba41bd5c368289f447de
Salamanilyas <95139252+SalmanIlya@users.noreply.github.com>
2022-11-30 11:10:24 +0500

    routes-for-products

--- Routes/ProductRoute.js
+++ Routes/ProductRoute.js
@@ -3,5 +3,9 @@ const Product = require("../Controllers/Product");
 const route = express.Router();
 
 route.post("/product", Product.createProduct);
+route.get("/", Product.GetAllProduct);
+route.get("/:id", Product.getSingleProduct);
+route.put("/:id", Product.UpdateProduct);
+route.delete("/:id", Product.Delete_Product);
 
 module.exports = route;
